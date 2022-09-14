2a80da8a00f2f7475c15ab3f134608aced2beb99
auvimal <auvimal@iu.edu>
2022-09-14 09:54:37 -0400

    Assignment 3

--- exercises/assignment3/solution_5.c
+++ exercises/assignment3/solution_5.c
@@ -4,9 +4,12 @@ int main(){
 	printf("Enter the number of rows: ");
 	int n, c;
 	scanf("%d",&n);
-	int num = n;
-	n /= 2;
+	if(n < 0 || n % 2 == 0){
+		printf("n has to be a postive odd number\n");
+		return 0;
+	}
 
+	n /= 2;
 	n++;
 
 	for(int i = 1; i <= n; i++){

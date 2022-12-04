bcbf6a9255b2670287213ec0d18c5f67d9d82158
auvimal <auvimal@iu.edu>
2022-12-04 12:37:52 -0500

    Final Project

---
+++ exercises/finalProject/sad.c
@@ -0,0 +1,171 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct node {
+   char *word;
+	
+   struct node *next;
+   struct node *prev;
+};
+
+//this link always point to first Link
+struct node *head = NULL;
+
+//this link always point to last Link 
+struct node *last = NULL;
+
+struct node *current = NULL;
+
+//is list empty
+bool isEmpty() {
+   return head == NULL;
+}
+
+void displayForward() {
+
+   //start from the beginning
+   struct node *ptr = head;
+
+   //navigate till the end of the list
+   printf("\n[ ");
+
+   while(ptr != NULL) {
+      printf("(%s) ",ptr->word);
+      ptr = ptr->next;
+   }
+
+   printf(" ]");
+}
+
+int length() {
+   int length = 0;
+   struct node *current;
+	
+   for(current = head; current != NULL; current = current->next){
+      length++;
+   }
+	
+   return length;
+}
+
+void insertFirst(char *word) {
+
+   //create a link
+   struct node *link = (struct node*) malloc(sizeof(struct node));
+   link->word = word;
+
+   if(isEmpty()) {
+      //make it the last link
+      last = link;
+   } else {
+      //update first prev link
+      head->prev = link;
+   }
+
+   //point it to old first link
+   link->next = head;
+
+   //point first to new first link
+   head = link;
+}
+
+void insertLast(char *word) {
+
+   //create a link
+   struct node *link = (struct node*) malloc(sizeof(struct node));
+   link->word = word;
+
+   if(isEmpty()) {
+      //make it the last link
+      last = link;
+   } else {
+      //make link a new last link
+      last->next = link;
+
+      //mark old last node as prev of new link
+      link->prev = last;
+   }
+
+   //point last to new last node
+   last = link;
+}
+
+struct node* delete(char *key) {
+
+   //start from the first link
+   struct node* current = head;
+   struct node* previous = NULL;
+
+   //if list is empty
+   if(head == NULL) {
+      return NULL;
+   }
+
+   //navigate through list
+   while(current->word != key) {
+      //if it is last node
+
+      if(current->next == NULL) {
+         return NULL;
+      } else {
+         //store reference to current link
+         previous = current;
+
+         //move to next link
+         current = current->next;
+      }
+   }
+
+   //found a match, update the link
+   if(current == head) {
+      //change first to point to next link
+      head = head->next;
+   } else {
+      //bypass the current link
+      current->prev->next = current->next;
+   }
+
+   if(current == last) {
+      //change last to point to prev link
+      last = current->prev;
+   } else {
+      current->next->prev = current->prev;
+   }
+
+   return current;
+}
+
+struct node* deleteLast() {
+   //save reference to last link
+   struct node *tempLink = last;
+
+   //if only one link
+   if(head->next == NULL) {
+      head = NULL;
+   } else {
+      last->prev->next = NULL;
+   }
+
+   last = last->prev;
+
+   //return the deleted link
+   return tempLink;
+}
+
+int main() {
+   insertFirst("Software");
+   insertFirst("Data Mining");
+   insertFirst("Database");
+   insertFirst("Search");
+
+   //add UI and checks to make sure it is LRU, it is simple for loops, prints, etc.
+   
+   // how tf do i make it into a least frequently used cache
+
+
+   displayForward();
+
+   return 0;
+}
